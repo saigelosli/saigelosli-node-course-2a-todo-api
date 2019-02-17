5480813cc058222a0d723d4a406964f1cc4bdb29
Saige Losli <saige.losli@gmail.com>
2019-02-17 13:03:08 -0800

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,13 +9,21 @@ const app = express();
 
 app.use( bodyParser.json() );
 
-app.post( "/todos", ( request, response   ) => {
+app.post( "/todos", ( request, response ) => {
   var todo = new Todo({
     text: request.body.text
   });
 
   todo.save().then( ( document ) => {
     response.send( document );
+  }, ( error ) => {
+    response.status( 400 ).send( error );
+  } );
+} );
+
+app.get( "/todos", ( request, response ) => {
+  Todo.find().then( ( todos ) => {
+    response.send( { todos } )
   }, ( error ) => {
     response.status( 400 ).send( error );
   } );
