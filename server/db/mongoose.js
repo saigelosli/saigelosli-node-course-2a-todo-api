cd76fb883b884c5c2a9f4dfe974dc11c03ebf709
Saige Losli <saige.losli@gmail.com>
2019-02-17 17:34:49 -0800

    Corrected mis-spelling in process.env variable name

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ const mongoose = require( "mongoose" );
 // Use the ES6 promises as opposed to a third-party promise library
 mongoose.Promise = global.Promise;
 // The basic connection info to initialize the library
-mongoose.connect( proceess.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp" );
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp" );
 
 module.exports = { mongoose };
