c7e62f8b87adbbc03dd1cf19903cb9ae607f543a
Saige Losli <saige.losli@gmail.com>
2019-02-17 16:39:55 -0800

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require( "express" );
 const bodyParser = require( "body-parser" );
+const { ObjectID } = require( "mongodb" );
 
 const { mongoose } = require( "./db/mongoose.js" );
 const { Todo } = require( "./models/todo.js" );
@@ -26,6 +27,32 @@ app.get( "/todos", ( request, response ) => {
     response.send( { todos } )
   }, ( error ) => {
     response.status( 400 ).send( error );
+  } );
+} );
+
+app.get( "/todos/:id", ( request, response ) => {
+  let id = request.params.id;
+
+  // Validate id using isValid
+  if ( !ObjectID.isValid( id ) ) {
+    // stop execution and respond with status 404, send empty body
+    return response.status( 404 ).send();
+  }
+
+  //  findById
+  Todo.findById( id ).then( ( todo ) => {
+    // success, but no todo
+    // send status 404 with empty body
+    if ( !todo ) {
+      return response.status( 404 ).send();
+    }
+
+    // if todo, send it back
+    response.send( { todo } );
+
+  } ).catch( ( error ) => {
+    // error - status 400, send empty body
+    response.status( 400 ).send();
   } );
 } );
 
