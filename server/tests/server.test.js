8007d6bbfb83fd1d21d6abf110a5f0939a38ad73
Saige Losli <saige.losli@gmail.com>
2019-02-19 11:39:13 -0800

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -319,3 +319,28 @@ describe( "POST /users/login", () => {
   } );
 
 } );
+
+describe( "DELETE /users/me/token", () => {
+
+  it( "Should remove auth token on logout", ( done ) => {
+
+    request( app )
+      .delete( "/users/me/token" )
+      .set( "x-auth", users[0].tokens[0].token )
+      .expect( 200 )
+      .expect( ( response ) => {
+        expect( response.headers["x-auth"] ).toNotExist();
+      } )
+      .end( ( error, response ) => {
+        if ( error ) {
+          return done( error )
+        }
+
+        User.findById( users[0]._id ).then( ( user ) => {
+          expect( user.tokens.length ).toBe( 0 );
+          done();
+        } ).catch( ( error ) => done( error ) );
+      } );
+  } );
+
+} );
